234d36ff88c3dfd21cd11f6053f050005d861474
Th3At0nic <islammdrahatul@gmail.com>
2021-06-20 21:31:48 +0600

    added dotenv for firebase

--- src/configs/firebase.config.js
+++ src/configs/firebase.config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyDG_PQ6Ja8o0yXEUtTvvtH9aeh26jv9QUM",
-  authDomain: "ema-john-th3at0nic.firebaseapp.com",
-  projectId: "ema-john-th3at0nic",
-  storageBucket: "ema-john-th3at0nic.appspot.com",
-  messagingSenderId: "867433180618",
-  appId: "1:867433180618:web:b9d5ec7f27cda0d49c3333",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
