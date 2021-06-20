2ffd70bc7ecd83dff743cf28e0723ade7901fcfd
Th3At0nic <islammdrahatul@gmail.com>
2021-06-20 17:49:54 +0600

    fixed an error of firebase config not found

---
+++ src/configs/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDG_PQ6Ja8o0yXEUtTvvtH9aeh26jv9QUM",
+  authDomain: "ema-john-th3at0nic.firebaseapp.com",
+  projectId: "ema-john-th3at0nic",
+  storageBucket: "ema-john-th3at0nic.appspot.com",
+  messagingSenderId: "867433180618",
+  appId: "1:867433180618:web:b9d5ec7f27cda0d49c3333",
+};
+
+export default firebaseConfig;
